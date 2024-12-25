89d65297a1e4a5bb7756126f3df92f5738964eaf
lukesarrazine@gmail.com <92548188+lukesarrazine@users.noreply.github.com>
2024-12-25 15:26:58 -0500

    Finishing up configuratino for cycling through colorschemes

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,36 +1,35 @@
 return {
-    -- Autocomplete plugins
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",   -- LSP source for nvim-cmp
-            "hrsh7th/cmp-buffer",     -- Buffer source
-            "hrsh7th/cmp-path",       -- Path source
-            "saadparwaiz1/cmp_luasnip", -- Snippet source
-            "L3MON4D3/LuaSnip",       -- Snippet engine
-        },
-        config = function()
-            local cmp = require("cmp")
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-j>"] = cmp.mapping.select_next_item(),
-                    ["<C-k>"] = cmp.mapping.select_prev_item(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                }),
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                    { name = "buffer" },
-                    { name = "path" },
-                    { name = "luasnip" },
-                }),
-            })
-        end,
-    },
+	-- Autocomplete plugins
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
+			"hrsh7th/cmp-buffer", -- Buffer source
+			"hrsh7th/cmp-path", -- Path source
+			"saadparwaiz1/cmp_luasnip", -- Snippet source
+			"L3MON4D3/LuaSnip", -- Snippet engine
+		},
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-j>"] = cmp.mapping.select_next_item(),
+					["<C-k>"] = cmp.mapping.select_prev_item(),
+					["<Tab>"] = cmp.mapping.confirm({ select = true }),
+					["<C-Space>"] = cmp.mapping.complete(),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "buffer" },
+					{ name = "path" },
+					{ name = "luasnip" },
+				}),
+			})
+		end,
+	},
 }
-
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,52 @@
+local theme_priority = 1000
+local current_colorscheme_index = 1
+local default_colorscheme = "bamboo"
+local colorschemes = { default_colorscheme, "rose-pine", "nightfox" }
+
+local function cycle_colorscheme()
+	current_colorscheme_index = current_colorscheme_index + 1
+	if current_colorscheme_index > #colorschemes then
+		current_colorscheme_index = 1
+	end
+
+	local colorscheme = colorschemes[current_colorscheme_index]
+	local success, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+	if success then
+		print("Switched to " .. colorscheme .. " colorscheme")
+	else
+		print("Failed to switch to " .. colorscheme .. " colorscheme")
+	end
+end
+
 return {
-   "EdenEast/nightfox.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.cmd("colorscheme nightfox")
-    end,
+	{
+		"EdenEast/nightfox.nvim",
+		lazy = false,
+		priority = theme_priority,
+	},
+	{
+		"ribru17/bamboo.nvim",
+		lazy = false,
+		priority = theme_priority,
+		config = function()
+			require("bamboo").setup({})
+		end,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		lazy = false,
+		priority = theme_priority + 1, -- run this last
+		config = function()
+			-- Automatically set colorscheme based on OS
+			local is_windows = vim.loop.os_uname().sysname:match("Windows")
+			if is_windows then
+				vim.cmd("colorscheme rose-pine")
+			else
+				vim.cmd("colorscheme " .. default_colorscheme)
+			end
+
+			vim.keymap.set("n", "<leader>cc", cycle_colorscheme, { desc = "Cycle through colorschemes" })
+		end,
+	},
 }
