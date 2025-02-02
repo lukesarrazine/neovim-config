c9147808614f0af1367892438a4c2bc61c33e0ca
lukesarrazine <92548188+lukesarrazine@users.noreply.github.com>
2025-02-01 20:40:57 -0500

    Fixing issues with tab completion and razor based lsp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -10,9 +10,19 @@ return {
         },
         snippets = { preset = 'luasnip' },
         keymap = {
-            preset = 'default',
+            preset = 'none',
 
-            ['<Tab>'] = { 'select_and_accept' },
+            ['<Tab>'] = {
+                function(cmp)
+                    if cmp.snippet_active() then
+                        return cmp.accept()
+                    else
+                        return cmp.select_and_accept()
+                    end
+                end,
+                'snippet_forward',
+                'fallback'
+            },
 
             ['<C-k>'] = { 'select_prev', 'fallback' },
             ['<C-j>'] = { 'select_next', 'fallback' },
